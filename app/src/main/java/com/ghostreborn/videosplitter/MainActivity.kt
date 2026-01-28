4af4e8ec7e8afbdd1ee625dfd4056ad967df5404
TorqueReborn <torquereborn@gmail.com>
2026-01-28 13:10:50 +0530

    Parse audio and video track index

--- app/src/main/java/com/ghostreborn/videosplitter/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/videosplitter/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ghostreborn.videosplitter
 
 import android.media.MediaExtractor
+import android.media.MediaFormat
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,32 @@ class MainActivity : AppCompatActivity() {
                 ?: throw IllegalArgumentException("Cannot open input URI")
             val extractor = MediaExtractor()
             extractor.setDataSource(inputFd.fileDescriptor)
+
+            var videoTrackIndex = -1
+            var audioTrackIndex = -1
+            var videoFormat: MediaFormat? = null
+            var audioFormat: MediaFormat? = null
+
+            for (i in 0 until extractor.trackCount) {
+                val format = extractor.getTrackFormat(i)
+                val mime = format.getString(MediaFormat.KEY_MIME)
+
+                when {
+                    mime?.startsWith("video/") == true -> {
+                        videoTrackIndex = i
+                        videoFormat = format
+                    }
+                    mime?.startsWith("audio/") == true -> {
+                        audioTrackIndex = i
+                        audioFormat = format
+                    }
+                }
+            }
+
+            Log.e("TAG", videoTrackIndex.toString())
+            Log.e("TAG", audioTrackIndex.toString())
+            Log.e("TAG", videoFormat.toString())
+            Log.e("TAG", audioFormat.toString())
         }
     }
 
