fabf8782541ebd68d4e9bf2d8a52bc2176d8152a
TorqueReborn <torquereborn@gmail.com>
2026-01-28 12:34:14 +0530

    Clicking button opens file picker to pick video

--- app/src/main/java/com/ghostreborn/videosplitter/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/videosplitter/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.ghostreborn.videosplitter
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val videoPicker = registerForActivityResult(ActivityResultContracts.GetContent()) {uri: Uri? ->
+        Toast.makeText(this@MainActivity, uri.toString(), Toast.LENGTH_LONG).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            videoPicker.launch("video/*")
         }
     }
 }
